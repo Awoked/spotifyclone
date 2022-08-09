d86572ba4b99f43f262eb56e702575fecf650581
Awoked <alper.kossay@gmail.com>
2022-08-09 18:59:29 +0300

    v1

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+function playlist1(){
+    let page = document.querySelector('#myIframe');
+    page.src = "html/playlist1.html";
+}
+function playlist2(){
+    let page = document.querySelector('#myIframe');
+    page.src = "html/playlist2.html";
+}
+function playlist3(){
+    let page = document.querySelector('#myIframe');
+    page.src = "html/playlist3.html";
+}
+function playlist4(){
+    let page = document.querySelector('#myIframe');
+    page.src = "html/playlist4.html";
+}
+function playlist5(){
+    let page = document.querySelector('#myIframe');
+    page.src = "html/playlist5.html";
+}
+function likedsongs() {
+    let page = document.querySelector('#myIframe');
+    page.src = "html/likedsongs.html";
+}
